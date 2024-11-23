04a33e183b3950b40ce9b8a5414834d5408cd8e8
Ivan Seibel <me@ivanseibel.dev>
2024-11-23 17:27:59 +0000

    feat: add Pagination component and integrate it into Orders page for improved navigation

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,3 +1,4 @@
+import { Pagination } from "@/components/pagination";
 import {
   Table,
   TableBody,
@@ -17,30 +18,33 @@ export function Orders() {
       </Helmet>
       <div className="flex flex-col gap-4">
         <h1 className="font-bold text-3xl tracking-tight">Orders</h1>
-      </div>
-      <div className="space-y-2.5">
-        <OrderTableFilters />
 
-        <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead className="w-[64px]"></TableHead>
-                <TableHead className="w-[140px]">Id</TableHead>
-                <TableHead className="w-[180px]">Finished at</TableHead>
-                <TableHead className="w-[140px]">Status</TableHead>
-                <TableHead>Customer</TableHead>
-                <TableHead className="w-[140px]">Amount</TableHead>
-                <TableHead className="w-[164px]"></TableHead>
-                <TableHead className="w-[132px]"></TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {[...Array(10)].map((_, index) => (
-                <OrderTableRow key={index} />
-              ))}
-            </TableBody>
-          </Table>
+        <div className="space-y-2.5">
+          <OrderTableFilters />
+
+          <div className="rounded-md border">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[64px]"></TableHead>
+                  <TableHead className="w-[140px]">Id</TableHead>
+                  <TableHead className="w-[180px]">Finished at</TableHead>
+                  <TableHead className="w-[140px]">Status</TableHead>
+                  <TableHead>Customer</TableHead>
+                  <TableHead className="w-[140px]">Amount</TableHead>
+                  <TableHead className="w-[164px]"></TableHead>
+                  <TableHead className="w-[132px]"></TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {[...Array(10)].map((_, index) => (
+                  <OrderTableRow key={index} />
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+
+          <Pagination pageIndex={0} totalCount={105} perPage={10} />
         </div>
       </div>
     </>
