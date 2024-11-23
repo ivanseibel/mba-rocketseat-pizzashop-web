b050d1a090b13ec870a91e4d396706886e3c48eb
Ivan Seibel <me@ivanseibel.dev>
2024-11-23 12:16:18 +0000

    feat: enhance AuthLayout with grid layout and footer for improved structure and branding

--- src/_layouts/auth.tsx
+++ src/_layouts/auth.tsx
@@ -1,10 +1,21 @@
+import { Pizza } from "lucide-react";
 import { Outlet } from "react-router-dom";
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Auth Header</h1>
-      <Outlet />
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="flex h-full flex-col justify-between border-foreground/5 border-r bg-muted p-10 text-muted-foreground">
+        <div className="flex items-center gap-3 text-foreground text-lg">
+          <Pizza className="h-5 w-5" />
+          <span className="font-semibold">pizza.shop</span>
+        </div>
+        <footer className="text-sm">
+          &copy; {new Date().getFullYear()} pizza.shop All rights reserved.
+        </footer>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <Outlet />
+      </div>
     </div>
   );
 }
