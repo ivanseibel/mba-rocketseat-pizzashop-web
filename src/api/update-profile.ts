9537b7849cde4844ba8448227a95be62f9c40129
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-12-02 21:45:31 +0000

    feat: allow description to be nullable in update profile API

--- src/api/update-profile.ts
+++ src/api/update-profile.ts
@@ -2,13 +2,12 @@ import { api } from "@/lib/axios";
 
 interface UpdateProfilePayload {
   name: string;
-  description: string;
+  description: string | null;
 }
 
 export async function updateProfile({
   name,
   description,
 }: UpdateProfilePayload) {
-  const { data } = await api.put("/profile", { name, description });
-  return data;
+  await api.put("/profile", { name, description });
 }
