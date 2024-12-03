cb125a3e0df648cdc72eb4a8c14c4d18b5c4eda4
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-12-03 19:13:47 +0000

    feat: implement pagination with pageIndex from search params and onPageChange handler

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -9,14 +9,30 @@ import {
 } from "@/components/ui/table";
 import { useQuery } from "@tanstack/react-query";
 import { Helmet } from "react-helmet-async";
+import { useSearchParams } from "react-router-dom";
+import { z } from "zod";
 import { OrderTableFilters } from "./order-table-filters";
 import { OrderTableRow } from "./order-table-row";
 
 export function Orders() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const pageIndex = z.coerce
+    .number()
+    .transform((page) => page - 1)
+    .parse(searchParams.get("page") ?? "1");
+
   const { data: result } = useQuery({
-    queryKey: ["orders"],
-    queryFn: getOrders,
+    queryKey: ["orders", pageIndex],
+    queryFn: () => getOrders({ pageIndex: pageIndex }),
   });
+
+  function handlePageChange(pageIndex: number) {
+    setSearchParams((prev) => {
+      prev.set("page", String(pageIndex + 1));
+      return prev;
+    });
+  }
 
   return (
     <>
@@ -35,8 +51,8 @@ export function Orders() {
                 <TableRow>
                   <TableHead className="w-[64px]"></TableHead>
                   <TableHead className="w-[140px]">Id</TableHead>
-                  <TableHead className="w-[180px]">Created at</TableHead>
-                  <TableHead className="w-[140px]">Status</TableHead>
+                  <TableHead className="w-[160px]">Created at</TableHead>
+                  <TableHead className="w-[180px]">Status</TableHead>
                   <TableHead>Customer</TableHead>
                   <TableHead className="w-[140px]">Amount</TableHead>
                   <TableHead className="w-[164px]"></TableHead>
@@ -51,7 +67,14 @@ export function Orders() {
             </Table>
           </div>
 
-          <Pagination pageIndex={0} totalCount={105} perPage={10} />
+          {result && (
+            <Pagination
+              pageIndex={result.meta.pageIndex}
+              totalCount={result.meta.totalCount}
+              perPage={result.meta.perPage}
+              onPageChange={handlePageChange}
+            />
+          )}
         </div>
       </div>
     </>
