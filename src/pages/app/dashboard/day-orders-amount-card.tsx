9512335e1b75a447fdb8e17bee7eb694b99c0e43
Ivan Luis Seibel <ivanseibel@gmail.com>
2024-12-06 18:25:55 +0000

    feat: implement daily orders amount card with data fetching and loading state

--- src/pages/app/dashboard/day-orders-amount-card.tsx
+++ src/pages/app/dashboard/day-orders-amount-card.tsx
@@ -1,7 +1,15 @@
+import { getDayOrdersAmount } from "@/api/get-day-orders-amount";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useQuery } from "@tanstack/react-query";
 import { Utensils } from "lucide-react";
 
 export function DayOrdersAmountCard() {
+  const { data: dayOrdersAmount } = useQuery({
+    queryKey: ["metrics", "day-orders-amount"],
+    queryFn: getDayOrdersAmount,
+  });
+
   return (
     <Card>
       <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
@@ -10,11 +18,35 @@ export function DayOrdersAmountCard() {
       </CardHeader>
 
       <CardContent className="space-y-1">
-        <span className="font-bold text-2xl tracking-tight">10</span>
-        <p className="text-muted-foreground text-xs">
-          <span className="text-rose-500 dark:text-rose-400">-3%</span> compared
-          to yesterday
-        </p>
+        {dayOrdersAmount ? (
+          <>
+            <span className="font-bold text-2xl tracking-tight">
+              {dayOrdersAmount.amount.toLocaleString("en-US")}
+            </span>
+            <p className="text-muted-foreground text-xs">
+              {dayOrdersAmount.diffFromYesterday >= 0 ? (
+                <>
+                  <span className="text-emeral-500 dark:text-emerald-400">
+                    +{dayOrdersAmount.diffFromYesterday}%
+                  </span>
+                  compared to yesterday
+                </>
+              ) : (
+                <>
+                  <span className="text-rose-500 dark:text-rose-400">
+                    {dayOrdersAmount.diffFromYesterday}%
+                  </span>
+                  compared to yesterday
+                </>
+              )}
+            </p>
+          </>
+        ) : (
+          <>
+            <Skeleton className="h-8" />
+            <Skeleton className="h-4" />
+          </>
+        )}
       </CardContent>
     </Card>
   );
