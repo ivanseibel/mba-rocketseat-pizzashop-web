47dc996dea949095b904d8f5065aafa601dc15c2
Ivan Seibel <me@ivanseibel.dev>
2024-12-08 15:01:48 +0000

    feat: add mock for dispatch order API

---
+++ src/api/mocks/dispatch-order-mock.ts
@@ -0,0 +1,15 @@
+import { http, HttpResponse } from "msw";
+import { DispatchOrderPayload } from "../dispatch-order";
+
+export const dispatchOrderMock = http.patch<DispatchOrderPayload, never, never>(
+  "/orders/:orderId/dispatch",
+  async ({ params }) => {
+    const { orderId } = params;
+
+    if (orderId === "error-order-id") {
+      return new HttpResponse(null, { status: 400 });
+    }
+
+    return new HttpResponse(null, { status: 204 });
+  },
+);
