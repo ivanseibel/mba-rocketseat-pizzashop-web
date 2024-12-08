eb0383f6f37cf179d6445fb573e24deb3480f0c3
Ivan Seibel <me@ivanseibel.dev>
2024-12-08 13:22:26 +0000

    feat: add mock for UpdateProfile API

---
+++ src/api/mocks/update-profile-mock.ts
@@ -0,0 +1,15 @@
+import { http, HttpResponse } from "msw";
+import { UpdateProfilePayload } from "../update-profile";
+
+export const updateProfileMock = http.put<never, UpdateProfilePayload>(
+  "/profile",
+  async ({ request }) => {
+    const { name } = await request.json();
+
+    if (name === "Super Pizza Shop") {
+      return new HttpResponse(null, { status: 204 });
+    }
+
+    return new HttpResponse(null, { status: 400 });
+  },
+);
