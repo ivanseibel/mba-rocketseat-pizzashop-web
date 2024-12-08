20894b4185857f1feb1aa8ed25675783675f2630
Ivan Seibel <me@ivanseibel.dev>
2024-12-08 16:27:30 +0000

    feat: remove example test and add sign-in e2e tests

---
+++ test/sign-in.e2e.spec.ts
@@ -0,0 +1,38 @@
+import { expect, test } from "@playwright/test";
+
+test("sign in successfully", async ({ page }) => {
+  await page.goto("/sign-in", {
+    waitUntil: "networkidle",
+  });
+
+  await page.getByLabel("Email").fill("johndoe@email.com");
+  await page.getByRole("button", { name: "Sign in" }).click();
+
+  const toast = page.getByText("A magic link has been sent to your email");
+
+  await expect(toast).toBeVisible();
+});
+
+test("sign in with wrong credentials", async ({ page }) => {
+  await page.goto("/sign-in", {
+    waitUntil: "networkidle",
+  });
+
+  await page.getByLabel("Email").fill("ddd@email.com");
+  await page.getByRole("button", { name: "Sign in" }).click();
+
+  const toast = page.getByText("Failed to send magic link. Please try again.");
+
+  await expect(toast).toBeVisible();
+});
+
+// navigate to sign-up page
+test("navigate to sign-up page", async ({ page }) => {
+  await page.goto("/sign-in", {
+    waitUntil: "networkidle",
+  });
+
+  await page.getByRole("link", { name: "New to pizza.shop? Sign up" }).click();
+
+  await expect(page).toHaveURL("/sign-up");
+});
