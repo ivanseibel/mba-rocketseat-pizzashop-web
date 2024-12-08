b90a4b9fbbf5400b4f337059f51758f871c34e13
Ivan Seibel <me@ivanseibel.dev>
2024-12-08 12:44:16 +0000

    feat: add additional mocks for revenue and order statistics

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -1,8 +1,24 @@
 import { setupWorker } from "msw/browser";
 import { env } from "../../env";
+import { getDailyRevenueInPeriodMock } from "./get-daily-revenue-in-period-mock";
+import { getDayOrdersAmountMock } from "./get-day-orders-amount-mock";
+import { getMonthCanceledOrdersAmountMock } from "./get-month-canceled-orders-amount-mock";
+import { getMonthOrdersAmountMock } from "./get-month-orders-amount-mock";
+import { getMonthRevenueMock } from "./get-month-revenue-mock";
+import { getPopularProductsMock } from "./get-popular-products-mock";
 import { signInMock } from "./sign-in-mock";
+import { signUpMock } from "./sign-up-mock";
 
-export const worker = setupWorker(signInMock);
+export const worker = setupWorker(
+  signInMock,
+  signUpMock,
+  getDayOrdersAmountMock,
+  getMonthOrdersAmountMock,
+  getMonthCanceledOrdersAmountMock,
+  getMonthRevenueMock,
+  getDailyRevenueInPeriodMock,
+  getPopularProductsMock,
+);
 
 export async function enableMSW() {
   if (env.MODE !== "test") return;
